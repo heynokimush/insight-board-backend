a6ad28da1e47694171910eff16e3e1180935bd8e
slee272 <slee272@naver.com>
2026-04-13 01:24:56 +0900

    install db set

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+TypeOrmModule.forRoot({
+  type: 'mysql',
+  host: 'RDS엔드포인트',
+  port: 3306,
+  username: 'root',
+  password: '123098',
+  database: 'insight',
+  autoLoadEntities: true,
+  synchronize: true, // 개발용만
+})
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
