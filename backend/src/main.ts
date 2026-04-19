32e1befaddca955d33e459c7817934f326247b43
slee272 <slee272@naver.com>
2026-04-19 16:26:56 +0900

    modify

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,12 +4,14 @@ import 'reflect-metadata';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+
   app.enableCors({
     origin: 'https://insightboard.duckdns.org', // 개발용 (나중엔 도메인 제한)
     credentials: true,
   });
   app.setGlobalPrefix('api');
+  
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 
 bootstrap();
