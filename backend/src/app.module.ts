ffeaab3a58c72423cc642d35730cdcf667ecbd1e
slee272 <slee272@naver.com>
2026-04-19 16:21:54 +0900

    feat: add nginx set

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,28 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { User } from './user/user.entity';
-import { Company } from './company/company.entity';
-import { Project } from './project/project.entity';
-import { UserProject } from './user-project/user-project.entity';
-import { Dashboard } from './dashboard/dashboard.entity';
-
-TypeOrmModule.forRoot({
-  type: 'mysql',
-  host: 'insight-board.cdyy6uq0m3zz.ap-southeast-2.rds.amazonaws.com',
-  port: 3306,
-  username: 'root',
-  password: '123098',
-  database: 'insight',
-  autoLoadEntities: true,
-  synchronize: true, // 개발용만
-  entities: [User, Company, Project, UserProject, Dashboard],
-})
-
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 어디서든 사용 가능
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // ⚠️ 개발용만!
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,5 +5,11 @@ import 'reflect-metadata';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  app.enableCors({
+    origin: 'https://insightboard.duckdns.org', // 개발용 (나중엔 도메인 제한)
+    credentials: true,
+  });
+  app.setGlobalPrefix('api');
 }
+
 bootstrap();
